47939fa22ecb250dae81663510353638488fada2
Krakaw <41575888+Krakaw@users.noreply.github.com>
2025-04-18 10:46:36 -0700

    Add grpc types

---
+++ src/client/index.ts
@@ -0,0 +1,9 @@
+import * as base_node from './base_node'
+import * as types from './types'
+import * as wallet from './wallet'
+import * as block from './block'
+import * as transaction from './transaction'
+import * as network from './network'
+import * as sidechain_types from './sidechain_types'
+
+export { base_node, types, wallet, block, transaction, network, sidechain_types }
--- src/lib.ts
+++ src/lib.ts
@@ -2,10 +2,11 @@ import { TariWalletCliClient } from './TariWalletCliClient';
 import { TariWalletGrpcClient } from './TariWalletGrpcClient';
 import { TariBaseNodeGrpcClient } from './TariBaseNodeGrpcClient';
 import * as utils from './utils';
-
+import * as client from './client';
 export {
     TariWalletCliClient,
     TariWalletGrpcClient,
     TariBaseNodeGrpcClient,
-    utils
+    utils,
+    client
 }; 
