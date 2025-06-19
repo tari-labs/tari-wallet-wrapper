65091589c6e6c5148358b3f469f2131e17acfd5c
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2025-06-19 12:27:33 -0700

    Krakaw add basenode methods (#5)
    * Adding some basenode tests
    
    * Add basenode methods (#4)
    
    * Fix proto (#3)
    
    * 0.1.0
    
    * Add function

--- src/TariBaseNodeGrpcClient.ts
+++ src/TariBaseNodeGrpcClient.ts
@@ -1,11 +1,16 @@
 import { ChannelCredentials, ServiceError } from "@grpc/grpc-js";
 
-import { BaseNodeClient, TipInfoResponse } from "./client/base_node.js";
+import { BaseNodeClient, BlockHeaderResponse, TipInfoResponse, GetBlocksRequest, GetNetworkStateRequest, GetNetworkStateResponse, StringValue, ValueAtHeightResponse } from "./client/base_node.js";
+import { HistoricalBlock } from "./client/block.js";
 import { Empty } from "./client/types.js";
+import Long from "long";
 
 export interface ITariBaseNodeGrpcClient {
   close(): void;
   getTipInfo(): Promise<TipInfoResponse>;
+  getBlocks(request: GetBlocksRequest): AsyncIterable<HistoricalBlock>;
+  checkConnectivity(): Promise<{ status: number }>;
+  getTokensInCirculation(heights: number[]): AsyncIterable<ValueAtHeightResponse>;
 }
 
 export class TariBaseNodeGrpcClient implements ITariBaseNodeGrpcClient {
@@ -30,6 +35,73 @@ export class TariBaseNodeGrpcClient implements ITariBaseNodeGrpcClient {
       });
     });
     return tipInfo;
+  }
+
+  public getBlocks(request: GetBlocksRequest): AsyncIterable<HistoricalBlock> {
+    const stream = this.client.getBlocks(request);
+    
+    return {
+      [Symbol.asyncIterator]() {
+        return {
+          async next() {
+            return new Promise((resolve, reject) => {
+              stream.on('data', (block: HistoricalBlock) => {
+                console.log('block', block);
+                resolve({ value: block, done: false });
+              });
+              stream.on('end', () => {
+                resolve({ value: undefined, done: true });
+              });
+              stream.on('error', (error) => {
+                reject(error);
+              });
+            });
+          }
+        };
+      }
+    };
+  }
+
+  public async getVersion(): Promise<string> {
+    const request = Empty.create();
+    const response = await new Promise<StringValue>((resolve, reject) => {
+      this.client.getVersion(request, (error: ServiceError | null, response: StringValue) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+    return response.value;
+  }
+
+  public async *getTokensInCirculation(heights: number[] = []): AsyncIterable<ValueAtHeightResponse> {
+    const request = GetBlocksRequest.create({ heights });
+    const stream = this.client.getTokensInCirculation(request);
+    
+    for await (const response of stream) {
+      yield response;
+    }
+  }
+
+  public async checkConnectivity(): Promise<{ status: number }> {
+    const request = GetNetworkStateRequest.create();
+    const response = await new Promise<GetNetworkStateResponse>((resolve, reject) => {
+      this.client.getNetworkState(request, (error: ServiceError | null, response: GetNetworkStateResponse) => {
+        console.log('getNetworkState', error, response);
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+
+    // Return a simple status object that matches the test expectations
+    return {
+      status: response.initialSyncAchieved ? 1 : 0
+    };
   }
 }
 
